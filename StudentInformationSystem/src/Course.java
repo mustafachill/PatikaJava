02979f4e008cd6cfc842694cfae15c0578aef318
Mustafa Çil <mustafacilbusiness@gmail.com>
2023-12-08 18:49:50 +0300

    A basic program for student information system

---
+++ StudentInformationSystem/src/Course.java
@@ -0,0 +1,11 @@
+public class Course {
+    String name;
+    String code;
+    String prefix;
+    double note;
+    String Teacher;
+
+    Course(String name,){}
+    void addTeacher(){}
+    void printTeacher(){}
+}
---
+++ StudentInformationSystem/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    String stuNo;
+    String classes;
+    double course1;
+    double course2;
+    double Course3;
+    double avarage;
+    boolean isPass;
+
+    Student(){}
+    void addBulkExamNote(){}
+    void isPass(){}
+    void calcAvarage(){}
+    void printNote(){}
+
+}
